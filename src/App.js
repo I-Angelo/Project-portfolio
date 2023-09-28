33024a3f57de869ffef267d0472c7da50f6f6979
I-Angelo <111244396+I-Angelo@users.noreply.github.com>
2023-09-28 19:03:01 -0400

    Add React Router routes with new React-dom version

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+//App.js
 import React from 'react';
 import './App.css';
 import Menu from './Menu'; // Import your Menu component
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import GitHubCarousel from './GitHubCarousel'; // Import your GitHubCarousel component
 
 function App() {
   return (
-    <div className="App">
-      <Menu /> {/* Render the Menu component */}
-      {/* More content */}
-    </div>
+    <Router>
+      <div className="App">
+        <Menu /> {/* Render the Menu component */}
+        {/* Define your routes inside the Routes component */}
+        <Routes>
+          {/* Route for the GitHubCarousel component */}
+          <Route path="/github-carousel" element={<GitHubCarousel />} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
